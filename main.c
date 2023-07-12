b264480cd62c757cb7a8837fc62bc6a6ecd92813
Andre Mendes <andre.f.mendes24@gmail.com>
2023-07-12 17:51:14 +0100

    Add text generation using a markov chain

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 #include <ctype.h>
 #define SV_IMPLEMENTATION
 #include "./sv.h"
@@ -16,7 +17,6 @@ void gt_print(GraphText* gt) {
     for(int i = 0; i < shlen(gt); ++i) {
         String_View* val = shget(gt, gt[i].key);
         printf("key: %s\n", gt[i].key);
-
         for(int j = 0; j < arrlen(val); j++) {
             printf("\tvalue[%d]: %s\n", j, val[j].data);
         }
@@ -26,13 +26,6 @@ void gt_print(GraphText* gt) {
 GraphText* gt_add_keyword(GraphText* gt, String_View keyword, String_View next_word) {
     // checks if word exists in hashTable
     String_View* keyword_vals = shget(gt, keyword.data);
-    if(keyword_vals != NULL) {
-        for(int i = 0; i < arrlen(keyword_vals); i++) {
-            if(sv_eq_ignorecase(keyword_vals[i], next_word) == true) {
-                return gt;
-            }
-        }
-    }
     arrpush(keyword_vals, next_word);
     shput(gt, keyword.data, keyword_vals);
     return gt;
@@ -43,6 +36,24 @@ GraphText* gt_fill(GraphText* gt, String_View* text) {
         gt = gt_add_keyword(gt, text[i-1], text[i]);
     }
     return gt;
+}
+
+
+void gt_generate_text(GraphText* gt, int num_words, int num_sentences, FILE* file) {
+    int gt_len = shlen(gt); 
+    for(int i = 0; i < num_sentences; i++) {
+        int key_idx = rand() % gt_len;
+        const char* curr_key = gt[key_idx].key;
+        for(int j = 0; j < num_words; j++) {
+            String_View* curr_value = shget(gt, curr_key);
+            int val_idx = rand() % arrlen(curr_value);
+            const char* word = curr_value[val_idx].data;
+            fputs(word, file);
+            fputs(" ", file);
+            curr_key = word;
+        }
+        fputs("\n\n", file);
+    }
 }
 
 String_View* read_file(char* filename) {
@@ -59,7 +70,7 @@ String_View* read_file(char* filename) {
         if(isspace(ch)) {
             word[i] = '\0';
             char* temp = malloc(strlen(word) + 1);
-            memcpy(temp, word, strlen(word) + 1);
+            strcpy(temp, word);
             arrput(text, SV(temp));
             i = 0;
         }
@@ -72,6 +83,7 @@ String_View* read_file(char* filename) {
 }
 
 int main(int argc, char** argv) {
+    srand(time(NULL));
     GraphText* gt = NULL;
     sh_new_arena(gt);
     
@@ -79,14 +91,25 @@ int main(int argc, char** argv) {
         fputs("Input error: Must be 3 or more arguments\n", stdout);
         return EXIT_FAILURE;
     }
-    String_View* file = read_file(argv[1]);
+    char* filename = argv[1];
+    int num_words = atoi(argv[2]);
+    int num_sentences = atoi(argv[3]);
+    String_View* file = read_file(filename);
     if(file == NULL) {
         perror("ERROR: POINTER NOT CREATED\n");
         return EXIT_FAILURE;
     }
-
+    if(num_words <= 0 || num_sentences <= 0) {
+        fputs("Input error: Arguments 2 and 3 must be positive and non-zero\n", stdout);
+        return EXIT_FAILURE;
+    }
     gt = gt_fill(gt, file);
 
-    gt_print(gt);
+    FILE* write_file = stdout;
+    if(argv[4] != NULL) {
+        write_file = fopen(argv[4], "w");
+    }
+    gt_generate_text(gt, num_words, num_sentences, write_file);
+    fclose(write_file);
     return 0;
 }
